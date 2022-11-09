a8dec46ee8a65caa90c79ee2e0cf3b189796add9
David Smellie <dsmellie@umich.edu>
2022-11-09 16:10:44 -0500

    Adding data and R script

---
+++ MechaCarChallenge.R
@@ -0,0 +1,14 @@
+library(dplyr)
+
+mpg_table <- read.csv(file='C:/Users/dsmel/OneDrive/Desktop/Bootcamp/MechaCar_Statistical_Analysis/MechaCar_mpg.csv',check.names=F,stringsAsFactors = F)
+lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,mpg_table)
+summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,mpg_table)) #generate summary statistics
+
+
+suspension_table <- read.csv(file='C:/Users/dsmel/OneDrive/Desktop/Bootcamp/MechaCar_Statistical_Analysis/Suspension_Coil.csv',check.names=F,stringsAsFactors = F)
+total_summary <- suspension_table  %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance= (sd(PSI)^2), SD = sd(PSI))
+
+
+?t.test() 
+t.test(suspension_table$PSI, mu=1500)
+t.test(suspension_table$PSI, mu=1500, subset = suspension_table$Manufacturing_Lot)
